3f39a57028eb8215443f1a4a8cbc584adc4a5300
Pedro Daniluz <pedrodaniluz04@gmail.com>
2024-03-09 11:31:03 -0300

    while ref + for loop

---
+++ src/aula3/ForTemperatura.java
@@ -0,0 +1,12 @@
+package aula3;
+
+public class ForTemperatura {
+    public static void main(String[] args) {
+        double celsius;
+
+        for(int fahrenheit = 100; fahrenheit <= 150; fahrenheit++) {
+            celsius = (double) 5 * (fahrenheit - 32) / 9;
+            System.out.printf("Fahrenheit: %dº ===> Celsius: %.2fº\n", fahrenheit, celsius);
+        }
+    }
+}
--- src/aula3/WhilePesquisa.java
+++ src/aula3/WhilePesquisa.java
@@ -4,36 +4,37 @@ import java.util.Scanner;
 
 public class WhilePesquisa {
     public static void main(String[] args) {
-        int qntFilho;
-        double valorSalario;
-
-        int totalFilhos = 0;
-        double totalSalario = 0;
-
-        int mostFilhos = 0;
-        double mostSalario = 0;
-
-        int cont = 0;
+        int qntFilho, totalFilhos = 0, mostFilhos = 0, cont = 0;
+        double valorSalario, totalSalario = 0, mostSalario = 0, quinhentos = 0;
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.print("Valor do Salário: R$");
-            valorSalario = scanner.nextDouble();
+        System.out.print("Valor do Salário: R$");
+        valorSalario = scanner.nextDouble();
+        while (valorSalario > 0) {
+            if (valorSalario <= 500) quinhentos ++;
             mostSalario = valorSalario > mostSalario ? valorSalario : mostSalario;
             totalSalario += valorSalario;
 
-            if (valorSalario <= 0) break;
-
-            System.out.print("Quantidade de filhos: ");
-            qntFilho = scanner.nextInt();
+            do {
+                System.out.print("Quantidade de filhos: ");
+                qntFilho = scanner.nextInt();
+            } while (qntFilho < 0);
             mostFilhos = qntFilho > mostFilhos ? qntFilho : mostFilhos;
             totalFilhos += qntFilho;
 
             cont++;
+
+            System.out.print("Valor do Salário: R$");
+            valorSalario = scanner.nextDouble();
         }
-        System.out.printf("Média Salarial: R$%.2f\n", totalSalario / cont);
-        System.out.printf("Média de Filhos: %.2f\n\n", totalFilhos * 1.0/ cont);
-        System.out.printf("Maior Salário: R$%.2f\n", mostSalario);
-        System.out.printf("Mais Filhos: %d\n\n", mostFilhos);
+        if(cont != 0) {
+            System.out.printf("Média Salarial: R$%.2f\n", totalSalario / cont);
+            System.out.printf("Maior Salário: R$%.2f\n", mostSalario);
+            System.out.printf("Salário até R$500,00: %.2f%%\n\n", quinhentos * 100.0 / cont);
+            System.out.printf("Média de Filhos: %.2f\n", totalFilhos * 1.0 / cont);
+            System.out.printf("Mais Filhos: %d\n\n", mostFilhos);
+        } else System.out.println("Não foi possível realizar nenhuma entrevista");
+
+        scanner.close();
     }
 }
